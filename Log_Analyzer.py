8b8d54d16d6444fdf313fb65de01277699f703b7
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-07-13 19:49:59 +0200

    Feat : Added for loop to pickup info and warnings

--- Log_Analyzer.py
+++ Log_Analyzer.py
@@ -7,3 +7,30 @@ print("=== Log Analyzer ===")
 print("====================")
 
 filename = input("\nEnter log file name : ")
+
+try:
+
+    with open(filename, "r") as file:
+        lines = file.readlines()
+
+    total_lines = len(lines)
+    info = 0
+    warnings = 0
+    errors = 0
+    failed_logins = 0
+
+    for line in lines:
+
+        line = line.upper()
+
+        if "INFO" in line:
+            info += 1
+
+        if "WARNING" in line:
+            warnings += 1
+
+
+    print("\n=== Analysis Results ===\n")
+    
+except FileNotFoundError:
+    print("\nError: File not found.")
